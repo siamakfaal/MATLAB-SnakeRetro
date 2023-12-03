a51723c8c3268e516f7b479bb6f56c3bc19fec90
Siamak Faal <siamak.faal@gmail.com>
2023-12-02 22:28:28 -0500

    fixes datasample

--- snake.m
+++ snake.m
@@ -40,16 +40,16 @@
 
 function snake
 
-global direction gamerun
+global dir gamerun
 
 worldSize = 21;
 pauseTime = 0.1;
 colors = [0.4 0.9 0.55; 0 0 0];
-k = round(worldSize/2);
+world_center = round(worldSize/2);
 
 world = zeros(worldSize);
-snakebody = [k*ones(1,6) ; k-5:k];
-world(snakebody(1,:),snakebody(2,:)) = 1;
+body = [world_center*ones(1,6) ; world_center-5:world_center];
+world(body(1,:), body(2,:)) = 1;
 
 f = figure('KeyPressFcn',@getKey); colormap(f,colors);
 ax = axes('Parent',f); hold(ax,'on');
@@ -65,18 +65,18 @@ scoreBoard = annotation('textbox','String',{'Score = 0','Time = 0'},...
 
 food = randi([2 worldSize-1],2,1);
 score = 0;
-direction = [0;1];
+dir = [0;1];
 gamerun = 1;
 
 tic;
 while(1)
-    newLoc = mod(snakebody(:,end) + direction ,worldSize+1);
-    newLoc(newLoc == 0) = worldSize*(sum(direction)==-1) +...
-        (sum(direction)==1);
-    world(snakebody(1,1),snakebody(2,1)) = 0;    
-    for i=1:length(snakebody)-1
-        snakebody(:,i) = snakebody(:,i+1);
-        if(snakebody(:,i+1) == newLoc)
+    newLoc = mod(body(:,end) + dir ,worldSize+1);
+    newLoc(newLoc == 0) = worldSize*(sum(dir)==-1) +...
+        (sum(dir)==1);
+    world(body(1,1),body(2,1)) = 0;    
+    for i=1:length(body)-1
+        body(:,i) = body(:,i+1);
+        if(body(:,i+1) == newLoc)
             gamerun = 0;
         end
     end
@@ -86,14 +86,15 @@ while(1)
         if(mod(score,10)==1)
             pauseTime = max(pauseTime - 0.01, 0.02);
         end
-        [food(1),food(2)] = ind2sub(size(world),...
-            datasample(find(world==0),1));
-        snakebody(:,end+1) = newLoc; %#ok<AGROW>
+        empty_rooms = find(world==0);
+        random_loc = randi([1, length(empty_rooms)],1);
+        [food(1),food(2)] = ind2sub(size(world), random_loc);
+        body(:,end+1) = newLoc; %#ok<AGROW>
     else
-        snakebody(:,end) = newLoc;
+        body(:,end) = newLoc;
     end
     
-    world(snakebody(1,end),snakebody(2,end)) = 1;
+    world(body(1,end),body(2,end)) = 1;
     world(food(1),food(2)) = 1;
     
     if(gamerun)
@@ -113,16 +114,16 @@ end
 end
 
 function getKey(~, eventdata)
-global direction gamerun
+global dir gamerun
 switch(eventdata.Key)
     case 'leftarrow'
-        if(direction(1)); direction = [ 0;-1]; end
+        if(dir(1)); dir = [ 0;-1]; end
     case 'rightarrow'
-        if(direction(1)); direction = [ 0; 1]; end
+        if(dir(1)); dir = [ 0; 1]; end
     case 'uparrow'
-        if(direction(2)); direction = [ 1; 0]; end
+        if(dir(2)); dir = [ 1; 0]; end
     case 'downarrow'
-        if(direction(2)); direction = [-1; 0]; end
+        if(dir(2)); dir = [-1; 0]; end
     case 'escape'
         gamerun = 0;
 end
